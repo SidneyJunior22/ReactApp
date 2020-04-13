812aebfe265a5803b7c451f7339458fdc5fe6194
Sidney Júnior <smjunior_22@hotmail.com>
2020-04-13 02:13:09 +0100

    Login PGE

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,11 @@
-import React, { useState } from 'react';
-import Header from './Header';
+import React from 'react';
+import Logon from './pages/logon'
 
+import './global.css';
 
 function App() {
-  const [counter, setCounter] = useState(0);
-  //Array [value, func]
-
-  function increment() {
-   setCounter(counter + 1);
-  }
-
-  return  (
-    <div>
-      <Header>
-        contador : {counter}
-      </Header>
-      <button onClick={increment}>Increment</button>
-    </div>
+ return (
+   <Logon/>
   );
 }
 
---
+++ frontend/src/pages/logon/index.js
@@ -0,0 +1,53 @@
+// import React, { Component } from 'react';
+// // import { FiLogIn } from 'react-icons/fi';
+// import {
+//   StyleSheet,
+//   Text,
+//   View,
+//   ImageBackground
+// } from 'react-native';
+
+// import backgroundImg from '../../assets/backimg.png'
+
+// export default class Example extends Component {
+//   render() {
+//     return (
+//       <ImageBackground source={backgroundImg} style={Styles.backgoundContainer}>
+
+//       </ImageBackground>
+//     );
+//   }
+// }
+
+// const styles = StyleSheet.create({
+//   backgoundContainer: {
+//     flex:1,
+//     justifyContent: 'center',
+//     alignItems: 'center',
+//   },
+// });
+// // import './styles.css';
+// // import logoImg from '../../assets/logo.svg'
+// // import backgroundImg from '../../assets/backimg.png'
+
+// // export default function Logon() {
+// //   return(
+// //     <div className="Logon-container">
+// //       <section className="form">
+// //         <img src={logoImg} alt="Logo"/>
+
+// //         <form >
+// //           <h1>Customer Login</h1>
+// //           <input placeholder="Your ID"/>
+// //           <button className="button" type="submit">Login</button>
+
+// //           <a href="/register">
+// //             <FiLogIn size={50} color="#FBFBFB"/>
+// //             I'm don't have an acount
+// //           </a>
+// //         </form>
+// //       </section>
+// //       <img className="image" src={backgroundImg} alt="Background image"/>
+// //     </div>
+// //   );
+// // }
