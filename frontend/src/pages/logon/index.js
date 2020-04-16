55f1ab662ede35458c18ffd04a1b3e2cd5e4bc97
Sidney Júnior <smjunior_22@hotmail.com>
2020-04-16 01:13:22 +0100

    connection with backend

--- frontend/src/pages/logon/index.js
+++ frontend/src/pages/logon/index.js
@@ -1,12 +1,27 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import { FiLogIn } from 'react-icons/fi';
 
+import api from '../../services/api';
+
 import './styles.css';
+
 import logoImg from '../../assets/logo.svg';
 import heroesImg from '../../assets/heroes.png';
 
 export default function Logon() {
+  const [id, setId] = useState('');
+
+  async function handleLogin(e) {
+    e.preventDefault();
+
+    try {
+      const response = await api.post('sessions', { id });
+      console.log(response.data.name);
+    }catch (err) {
+      alert('Login failed, try again.');
+    }
+  }
   return(
     <div className="Logon-container">
       <section className="form">
